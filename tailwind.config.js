43e99b2bb7552d41b04eb017c730dab4498495c3
Raphael Cunha <rbcunhadesign@gmail.com>
2019-06-19 14:06:49 -0400

    Add tailwind to CSS

---
+++ tailwind.config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  theme: {
+    colors: require('./src/tailwind/colors'),
+
+    screens: {
+      sm: '32em',
+      md: '48em',
+      lg: '64em',
+      xl: '80em',
+    },
+
+    spacing: {
+      px: '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
+    },
+
+    extend: {}
+  },
+
+  variants: {
+    backgroundColor: ['hover', 'focus'],
+    borderColor: ['hover', 'focus'],
+    borderRadius: [],
+    borderStyle: [],
+    borderWidth: [],
+    boxShadow: ['hover', 'focus'],
+    fontSmoothing: [],
+    fontWeight: ['hover', 'focus'],
+    opacity: [],
+    outline: ['focus'],
+    overflow: [],
+    stroke: [],
+    textColor: ['hover', 'focus'],
+    textDecoration: ['hover', 'focus'],
+    textTransform: [],
+    zIndex: [],
+  },
+
+  plugins: [],
+
+  corePlugins: {
+    preflight: false,
+    container: false,
+    appearance: false,
+    backgroundAttachment: false,
+    backgroundPosition: false,
+    backgroundRepeat: false,
+    backgroundSize: false,
+    float: false,
+    fontFamily: false,
+    objectFit: false,
+    objectPosition: false,
+    pointerEvents: false,
+    userSelect: false,
+    whitespace: false,
+  },
+};
