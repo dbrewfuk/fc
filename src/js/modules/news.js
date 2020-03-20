45be3a235fe8ff7dd1f851714b926963b71d5e30
Hampton <bram.hampton@kctcs.edu>
2020-03-20 11:03:42 -0400

    Added news js  file

---
+++ src/js/modules/news.js
@@ -0,0 +1,5 @@
+let sortUp = document.getElementById("ou_orderAsc");
+let sortDown = document.getElementById("ou_orderDesc");
+
+sortUp.src = "https://webassets.kctcs.edu/_resources/images/newsSortUp.gif";
+sortDown.src = "https://webassets.kctcs.edu/_resources/images/newsSortDown.gif";
