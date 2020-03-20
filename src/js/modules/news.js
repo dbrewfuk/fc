5f9abdf34f1aacb7318eb3f1e6a71d7cbee3896b
Bram Hampton <bram.hampton@kctcs.edu>
2020-03-20 15:34:16 +0000

    Update news.js

--- src/js/modules/news.js
+++ src/js/modules/news.js
@@ -4,7 +4,7 @@ let sortDown = document.getElementById("ou_orderDesc");
 sortUp.src = "https://webassets.kctcs.edu/_resources/images/newsSortUp.gif";
 sortDown.src = "https://webassets.kctcs.edu/_resources/images/newsSortDown.gif";
 
-document.getElementById("ou_feed").addEventListener("onload", function(){
+document.getElementById("ou_feed").addEventListener("onclick", function(){
     sortUp.src = "https://webassets.kctcs.edu/_resources/images/newsSortUp.gif";
     sortDown.src = "https://webassets.kctcs.edu/_resources/images/newsSortDown.gif";
 })
